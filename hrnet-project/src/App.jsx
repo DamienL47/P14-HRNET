41692fefa96da59db0e571a2b3c84e478ec317c6
DamienL47 <damien.lataste@lapiscine.pro>
2023-12-29 08:44:30 +0100

    uninstall test package

--- hrnet-project/src/App.jsx
+++ hrnet-project/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
+import { MyDateTimePicker } from "oraclion-date-picker";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<div>Home</div>} />
-    </Routes>
+    <div>
+      <h1>HRNet Project</h1>
+      <MyDateTimePicker labelText={"Selectionner une date: "} />
+    </div>
+    // <Routes>
+    //   <Route
+    //     path="/"
+    //     element={<DatePicker labelText={"Selectionner une date: "} />}
+    //   />
+    // </Routes>
   );
 }
 
